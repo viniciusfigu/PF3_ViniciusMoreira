2bf31da0f4992c57f927d355287d8efb70745970
viniciusfigu <79717926+viniciusfigu@users.noreply.github.com>
2021-11-23 11:21:59 -0300

    Add files via upload

--- fuscript.js
+++ fuscript.js
@@ -2,21 +2,24 @@ const calcular = document.getElementById('calcular');
 
 
 function investir(){
-    const nome = document.getElementById('nome').value
-    const mensal = document.getElementById('Rmensal').value
-    const tjuros = document.getElementById('Txjuros').value
-    const meses = document.getElementById('Nparcelas').value
-    const juros = tjuros/100    
-    const resultado = document.getElementById("resultado")
+    const nome = document.getElementById('nome').value;
+    const mensal = document.getElementById('Rmensal').value;
+    const tjuros = document.getElementById('Txjuros').value;
+    const meses = document.getElementById('Nparcelas').value;
+    const juros = tjuros/100;
+    const resultado = document.getElementById("resultado");
 
    
-    let p = ((1+juros)**meses-1)/juros 
-    let vf = mensal*p.toFixed(2)
-  
-    resultado.innerHTML  = `${nome}, se você aplicar Renda mensal de R$ ${mensal}, 
+    const p = ((1+juros)**meses-1)/juros;
+    const vf = mensal*p.toFixed(2);
+    if (nome !=='' && mensal !=='' && tjuros !=='' && meses !=='')
+  {
+    resultado.textContent  = `${nome}, se você aplicar Renda mensal de R$ ${mensal}, 
     com uma taxa de juros de ${tjuros}% ao mês,
     durante ${meses} meses,
-    acumulará uma poupança de R$ ${vf}`
+    acumulará uma poupança de R$ ${vf}`;
+  }
+  else{resultado.textContent = 'Para calcular, preencha todos os campos.';}
     
 }
 
